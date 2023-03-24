5575e8cdc02a901be8754e7c33ecf3af0ed5a9a3
Maxim Marinov <MMMarinov20@codingburgas.bg>
2023-03-24 15:58:10 +0200

    feat(backend): initialize game manager functions

--- app/include/GameManager.hpp
+++ app/include/GameManager.hpp
@@ -1,12 +1,31 @@
 #pragma once
-
+#include <iostream>
+#include <raylib.h>
+#include <vector>
 
 class GameManager
 {
 public:
+	enum SCENE {
+		NO_SCENE,
+		MENU,
+		GAME
+	};
+
+	SCENE CurrentScene = NO_SCENE;
 	static GameManager* GetInstance();
+	void Update();
+	void LoadScene(SCENE sceneID, std::vector<std::string> textures, std::vector<std::string> hoverEffects, std::vector<Vector2> positions);
+	void DrawTextures();
+	void DrawButtons();
+	void UnloadScene();
 	~GameManager();
 private:
 	static GameManager* instance;
+	std::vector<Texture2D> m_Textures;
+	std::vector<Vector2> m_TexturePositions;
+	std::vector<Texture2D> m_Buttons;
+	std::vector<Texture2D> m_OnHoverButtons;
+	std::vector<Vector2> m_ButtonPositions;
 	GameManager();
 };
