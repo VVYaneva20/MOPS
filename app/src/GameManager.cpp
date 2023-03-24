d89c52419cc0ed4a655543cecfda34f933d18a65
Maxim Marinov <MMMarinov20@codingburgas.bg>
2023-03-24 15:58:10 +0200

    feat(backend): initialize game manager functions

--- app/src/GameManager.cpp
+++ app/src/GameManager.cpp
@@ -7,6 +7,16 @@ GameManager::GameManager() {};
 
 GameManager::~GameManager() {};
 
+void GameManager::Update() {}
+
+void GameManager::LoadScene(SCENE sceneID, std::vector<std::string> textures, std::vector<std::string> hoverEffects, std::vector<Vector2> positions) {}
+
+void GameManager::DrawTextures() {}
+
+void GameManager::DrawButtons() {}
+
+void GameManager::UnloadScene() {}
+
 GameManager *GameManager::GetInstance() {
 	if (!instance) instance = new GameManager();
 	return instance;
