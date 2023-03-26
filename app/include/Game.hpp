34483f5bfd744caf1fe58ca2eddd5d94e6468681
SHHristov20 <SHHristov20@codingburgas.bg>
2023-03-26 16:45:15 +0300

    feat(frontend): draw the 3D model of every element in the info tab

--- app/include/Game.hpp
+++ app/include/Game.hpp
@@ -11,9 +11,4 @@ public:
 	~Game();
 private:
 	GameManager* gameManager = GameManager::GetInstance();
-	Model model = LoadModel("./assets/Lab/hydrogen.glb");
-	Camera camera = { 0 };
-	void SetCameraSettings(Camera &camera);
-	float yaw = 0.0f;
-	void Update3D();
 };
--- app/src/Game.cpp
+++ app/src/Game.cpp
@@ -3,14 +3,12 @@
 Game::Game() {
 	gameManager->LoadScene(gameManager->SCENE::GAME, { "Lab/Lab.png" }, { {0, 0} });
 	gameManager->LoadButtons({ "Lab/PC.png" }, { "Lab/PCHover.png" }, { { 1580, 575 } });
-	this->SetCameraSettings(this->camera);
 	while (gameManager->CurrentScene == gameManager->SCENE::GAME && !gameManager->GetShouldClose())
 	{
 
 		BeginDrawing();
 		ClearBackground(BLUE);
 		gameManager->Update();
-		this->Update3D();
 		EndDrawing();
 
 		if (IsKeyPressed(KEY_ESCAPE))
@@ -27,26 +25,6 @@ Game::Game() {
 			break;
 		}
 	}
-}
-
-void Game::SetCameraSettings(Camera &camera) {
-	this->camera.position = Vector3{ 0.0f, 10.0f, 10.0f };
-	this->camera.target = Vector3{ 0.0f, 0.0f, 0.0f };
-	this->camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
-	this->camera.fovy = 10.0f;
-	this->camera.projection = CAMERA_PERSPECTIVE;
-	SetCameraMode(this->camera, CAMERA_FREE);
-}
-
-void Game::Update3D() {
-	UpdateCamera(&this->camera);
-	this->model.transform = MatrixRotateXYZ(Vector3{ DEG2RAD * 1, DEG2RAD * yaw, DEG2RAD * 1 });
-	this->camera.position = Vector3{ 0.0f, 10.0f, 10.0f };
-	this->yaw += 0.2f;
-	
-	BeginMode3D(this->camera);
-	DrawModel(this->model, { 1.8f, 1.0f, 0.0f }, 0.5, WHITE);
-	EndMode3D();
 }
 
 Game::~Game() {}
