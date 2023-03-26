fd86adc14944d1e2eefefcec639037e010918790
Maxim Marinov <MMMarinov20@codingburgas.bg>
2023-03-26 12:09:10 +0300

    feat(backend): implement setter for all elements using JSON

--- app/include/TableManager.hpp
+++ app/include/TableManager.hpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <raylib.h>
 #include <GameManager.hpp>
+#include <json.h>
+#include <fstream>
 
 class TableManager {
 public:
@@ -13,13 +15,27 @@ private:
 	GameManager* gameManager = GameManager::GetInstance();
 	
 	struct PeriodicElement {
-		std::string name;
-		std::string symbol;
-		Vector2 position;
+		const char* name;
+		const char* appearance;
+		float atomicMass;
+		int boil;
+		const char* category;
+		float density;
+		float melt;
+		float molarHeat;
+		int atomicNumber;
+		int period;
+		int group;
+		const char* phase;
+		//const char* summary;
+		const char* symbol;
+		int posX;
+		int posY;
 	};
 	
 	Texture2D tableOutline = LoadTexture((gameManager->GetAssetPath() + "Table/TableOutline.png").c_str());
 	void DrawPeriodicTable(std::vector<PeriodicElement> elements);
-
+	std::vector<PeriodicElement> setPeriodicElements(std::vector<PeriodicElement> &elements);
+	
 	std::vector<PeriodicElement> m_Elements;
 };
