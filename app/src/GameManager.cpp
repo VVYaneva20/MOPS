972525931a8f84357d8c63bfd1feade619f1ca43
Maxim Marinov <MMMarinov20@codingburgas.bg>
2023-03-26 16:15:06 +0300

    feat(frontend): display information for every element

--- app/src/GameManager.cpp
+++ app/src/GameManager.cpp
@@ -7,6 +7,9 @@ GameManager::GameManager() {
 	SetConfigFlags(FLAG_VSYNC_HINT);
 	InitWindow(this->m_ScreenSize.x, this->m_ScreenSize.y, "MOPS CHEMISTRY LAB");
 	SetExitKey(KEY_NULL);
+	this->Arial = LoadFontEx((this->GetAssetPath() + "Arial.ttf").c_str(), 1000, 0, 0);
+	this->ArialBold = LoadFontEx((this->GetAssetPath() + "arialbd.ttf").c_str(), 100, 0, 0);
+	SetTextureFilter(this->ArialBold.texture, TEXTURE_FILTER_POINT);
 	//ToggleFullscreen();
 };
 
--- app/src/TableManager.cpp
+++ app/src/TableManager.cpp
@@ -2,7 +2,7 @@
 
 TableManager::TableManager() {
 	this->m_Elements = setPeriodicElements(this->m_Elements);
-	this->Update();
+	this->m_SelectedElement = m_Elements[0];
 }
 
 TableManager::~TableManager() {}
@@ -10,21 +10,58 @@ TableManager::~TableManager() {}
 void TableManager::Update() {
 	DrawTextureEx(this->tableOutline, { 30, 300 }, 0, 1, WHITE);
 	DrawPeriodicTable(this->m_Elements);
+	DisplayInfo(this->m_SelectedElement);
 }
 
 void TableManager::DrawPeriodicTable(std::vector<TableManager::PeriodicElement> elements) {
 	for (size_t i = 0; i < elements.size(); i++) {
-		DrawTexture(elements[i].texture, elements[i].posX, elements[i].posY, WHITE);
+		DrawTextureEx(elements[i].texture, { float(elements[i].posX), float(elements[i].posY) }, 0, 0.1625, WHITE);
 		if (!elements[i].unlocked)
 		{
-			DrawRectangle(elements[i].posX, elements[i].posY, 63, 65, Fade(BLACK, 0.7f));
 			DrawTexture(this->padlock, elements[i].posX + 14, elements[i].posY + 15, WHITE);
+			DrawRectangle(elements[i].posX, elements[i].posY, 63, 65, Fade(BLACK, 0.7f));
 		}
-		if (CheckCollisionPointRec(GetMousePosition(), { (float)elements[i].posX, (float)elements[i].posY, (float)elements[i].texture.width, (float)elements[i].texture.height })) {
+		if (CheckCollisionPointRec(GetMousePosition(), { (float)elements[i].posX, (float)elements[i].posY, 65, 65 })) {
 			// draw a border around the element
-			DrawRectangleLinesEx({ (float)elements[i].posX, (float)elements[i].posY, (float)elements[i].texture.width, (float)elements[i].texture.height }, 4, ORANGE);
+			SetMouseCursor(MOUSE_CURSOR_POINTING_HAND);
+			DrawRectangleLinesEx({ (float)elements[i].posX, (float)elements[i].posY, 65, 65 }, 4, ORANGE);
+			if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+				std::cout << elements[i].name;
+				this->m_SelectedElement = elements[i];
+			}
+		}
+		SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+	}
+}
+
+void TableManager::DisplayInfo(TableManager::PeriodicElement element) {
+	DrawTextureEx(element.texture, { 1500, 150 }, 0, 0.35, WHITE);
+	DrawTextEx(gameManager->ArialBold, (element.name).c_str(), { 1650, 200 }, 35, 0.4, WHITE);
+	DrawTextEx(gameManager->ArialBold, ("Group: " + std::to_string(element.group)).c_str(), { 1500, 320 }, 30, 0.2, WHITE);
+	DrawTextEx(gameManager->ArialBold, ("Period: " + std::to_string(element.period)).c_str(), { 1500, 360 }, 30, 0.2, WHITE);
+	DrawTextEx(gameManager->ArialBold, ("Category: " + element.category).c_str(), { 1500, 400 }, 30, 0.2, WHITE);
+	DrawTextEx(gameManager->ArialBold, ("Phase: " + element.phase).c_str(), { 1500, 440 }, 30, 0.2, WHITE);
+	DrawTextEx(gameManager->ArialBold, ("Appearance: " + element.appearance).c_str(), { 1500, 480 }, 30, 0.2, WHITE);
+	DrawTextEx(gameManager->ArialBold, ("Atomic mass: " + std::to_string(element.atomicMass)).c_str(), { 1500, 520 }, 30, 0.2, WHITE);
+	std::string summary = element.summary;
+	for (size_t i = 0, j = 0; i < summary.length(); i++, j++) {
+		if (i > 300) {
+			summary.insert(i, "...");
+			break;
+		}
+		if (j > 33) {
+			if (summary[i] == ' ') {
+				summary.insert(i + 1, "\n");
+				j = 0;
+			}
+			if (summary[i] == ',' || summary[i] == '.' || summary[i] == ';' || summary[i] == ':') {
+				summary.erase(i + 1, 1);
+				summary.insert(i + 1, "\n");
+				j = 0;
+			}
 		}
 	}
+	DrawTextEx(gameManager->ArialBold, summary.substr(0, 304).c_str(), { 1500, 580 }, 22, 0.2, WHITE);
 }
 
 std::vector<TableManager::PeriodicElement> TableManager::setPeriodicElements(std::vector<PeriodicElement> &elements) {
