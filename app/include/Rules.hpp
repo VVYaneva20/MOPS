69db3192471c4fb535f4c8fa872c6504d3e87fb3
PDMatsaliev20 <PDMatsaliev20@codingburgas.bg>
2023-03-30 23:39:40 +0300

    feat(backend): implement back/next buttons functionality

--- app/include/Rules.hpp
+++ app/include/Rules.hpp
@@ -8,9 +8,21 @@ public:
 	~Rules();
 private:
 	GameManager* gameManager = GameManager::GetInstance();
-
+	
 	std::vector<std::string> m_Buttons = { "Rules/Next.png", "Rules/Back.png" };
 	std::vector<std::string> m_ButtonsHover = { "Rules/NextHover.png", "Rules/BackHover.png" };
-	std::vector<Vector2> m_ButtonPositions = { {1320, 781}, {314, 781} };
+	std::vector<Vector2> m_ButtonPositions = { {1360, 812}, {273, 812} };
 	std::vector<std::string> m_ButtonNames = { "NEXT", "BACK" };
+};
+
+class SecondPage {
+public:
+	SecondPage();
+	~SecondPage();
+private:
+	GameManager* gameManager = GameManager::GetInstance();
+	std::vector<std::string> m_Buttons = { "Rules/Back.png" };
+	std::vector<std::string> m_ButtonsHover = { "Rules/BackHover.png" };
+	std::vector<Vector2> m_ButtonPositions = { {273, 812} };
+	std::vector<std::string> m_ButtonNames = { "BACK" };
 };
