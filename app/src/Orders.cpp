5964d244443397f7df94df10f645a04712fd4571
Maxim Marinov <MMMarinov20@codingburgas.bg>
2023-03-30 12:33:17 +0300

    feat(backend): create timer for order generation

--- app/src/Orders.cpp
+++ app/src/Orders.cpp
@@ -1,9 +1,21 @@
 #include <Orders.hpp>
 
-Orders::Orders() {
-	
-}
+Orders::Orders() {}
+
+Orders::~Orders() {}
 
 void Orders::Update() {
 	DrawTexture(this->background, 0, 0, WHITE);
+}
+
+Orders::Order Orders::generateOrder() {
+    std::chrono::seconds duration(10);
+    std::chrono::steady_clock::time_point endTime = this->startTime + duration;
+	
+	if (std::chrono::steady_clock::now() >= endTime) {
+		this->startTime = std::chrono::steady_clock::now();
+		endTime = this->startTime + duration;
+		std::cout << "Order generated" << std::endl;
+	}
+    return {};
 }
