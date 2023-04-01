a4da6cccf98cb0e9bd70698876535cf86a688c21
SHHristov20 <SHHristov20@codingburgas.bg>
2023-04-01 23:58:42 +0300

    fix: fix order optimization

--- app/include/Orders.hpp
+++ app/include/Orders.hpp
@@ -17,14 +17,14 @@ public:
 	};
 	struct Order {
 		std::string product;
-		std::vector<Reactants> formula;
+		std::string formula;
+		std::vector<Reactants> reactants;
 		std::string status;
 		std::string buyer;
-		std::string date;
 		float price;
 	};
 	std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
-	Order generateOrder();
+	void generateOrder();
 private:
 	GameManager* gameManager = GameManager::GetInstance();
 	Texture2D background = LoadTexture((gameManager->GetAssetPath() + "Orders/Orders.png").c_str());
