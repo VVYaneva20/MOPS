bad20b12569cf408d6d7632c4f9466690a8ce671
SHHristov20 <SHHristov20@codingburgas.bg>
2023-04-01 15:07:20 +0300

    feat(frontend/backend): implement multiple page inventory

--- app/include/Game.hpp
+++ app/include/Game.hpp
@@ -22,6 +22,10 @@ private:
 	Orders* orders = new Orders();
 	Texture2D HUD = LoadTexture((gameManager->GetAssetPath() + "Lab/HUD.png").c_str());
 	Texture2D Slot = LoadTexture((gameManager->GetAssetPath() + "Lab/InventorySlot.png").c_str());
+	Texture2D Prev = LoadTexture((gameManager->GetAssetPath() + "Lab/Prev.png").c_str());
+	Texture2D PrevHover = LoadTexture((gameManager->GetAssetPath() + "Lab/PrevHover.png").c_str());
+	Texture2D Next = LoadTexture((gameManager->GetAssetPath() + "Lab/Next.png").c_str());
+	Texture2D NextHover = LoadTexture((gameManager->GetAssetPath() + "Lab/NextHover.png").c_str());
 	std::vector<InventorySlot> inventory;
 	void Update();
 	void setInventory();
@@ -29,4 +33,5 @@ private:
 	bool selected = false;
 	bool isInventoryOpen = false;
 	int m_Balance = gameManager->GetBalance();
+	int page = 1;
 };
